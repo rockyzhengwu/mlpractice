8a4f968e222f6d139f49850a8a514183aed54b30
rocky <zhengwu314@163.com>
2016-11-22 22:47:17 +0800

    knn digist

--- test/knn_test.py
+++ test/knn_test.py
@@ -2,6 +2,7 @@
 # -*-coding=utf-8-*-
 
 import numpy as np
+import os
 
 from algorithm.knn import KNN
 
@@ -25,6 +26,46 @@ def test_knn():
     result = model.predict([0, 0])
     print(result)
 
+def hand_write():
+    base_path = "/home/rocky/Documents/book/machinelearninginaction/Ch02/digits"
+    x = []
+    y = []
+    train_path = os.path.join(base_path, "trainingDigits")
+    for ts in os.listdir(train_path):
+        y.append(ts.split("_")[0])
+        tmp_list =[]
+        for lin in open(os.path.join(train_path, ts)):
+            lin = lin.strip("\n")
+            if not lin:
+                continue
+            tmp_list.extend(float(num) for num in list(lin))
+        x.append(tmp_list)
+    x = np.array(x)
+
+    model = KNN(3)
+    model.fit(x, y)
+    test_path = os.path.join(base_path, "testDigits")
+    acc_count = 0
+    counter = 0
+    for ts in os.listdir(test_path):
+        counter += 1
+        test_x = []
+        test_y = ts.split("_")[0]
+        for lin in open(os.path.join(train_path, ts)):
+            lin = lin.strip("\n")
+            if not lin :
+                continue
+            test_x.extend(float(num) for num in list(lin))
+        test_x = np.array(test_x)
+        test_x.reshape(1024,1)
+
+        predict_y = model.predict(np.array(test_x))
+        print("redict:%s, acture:%s"%(predict_y, test_y))
+        if predict_y==test_y:
+            acc_count += 1
+        print ("acc:%.4f"%(acc_count/counter))
+
 
 if __name__ == '__main__':
-    test_knn()
+    # test_knn()
+    hand_write()
